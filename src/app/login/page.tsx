ec34bb3ae126bf0b3f93eb5c9df61996170f4129
Rizaldi Abyannata <aldizar255@gmail.com>
2025-09-14 12:50:40 +0800

    feat: add UI components for collapsible, dropdown menu, separator, sheet, sidebar, skeleton, and tooltip
    - Implemented Collapsible component with trigger and content.
    - Created DropdownMenu component with various subcomponents including items, checkboxes, and radio buttons.
    - Added Separator component for visual separation in UI.
    - Developed Sheet component for modal-like overlays with header, footer, and content.
    - Introduced Sidebar component with context for managing open/closed state and mobile responsiveness.
    - Added Skeleton component for loading states.
    - Implemented Tooltip component with provider and content for enhanced user guidance.
    - Created useIsMobile hook to manage mobile state detection.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,11 +7,9 @@ export default function LoginPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
+          <a href="#" className="flex items-center gap-2 font-medium text-xl font-bold">
+             <img src="/logo.png" alt="Logo" width={16} height={16} className="size-10" />
+            HapiBite
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
@@ -22,7 +20,7 @@ export default function LoginPage() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.svg"
+          src="https://placeholder.pics/svg/300"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
