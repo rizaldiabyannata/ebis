bb601a8baf810cf3b39b53b5f145b204cd24c69d
Rizaldi Abyannata <aldizar255@gmail.com>
2025-09-15 20:39:45 +0800

    Perubahan admin products page (seharusnya di branch ui/products-admin-page)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,10 +5,7 @@ export default function LoginPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a
-            href="#"
-            className="flex items-center gap-2 font-medium text-xl font-bold"
-          >
+          <a href="#" className="flex items-center gap-2 font-medium text-xl">
             <img src="/logo.png" alt="Logo" className="size-10" />
             HepiBite
           </a>
