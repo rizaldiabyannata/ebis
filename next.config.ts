b1b65ab1ae16a13692aa55766ee387556496f1f5
Rizaldi Abyannata <aldizar255@gmail.com>
2025-09-24 03:14:38 +0800

    chore: add testing scripts and dependencies for Jest and Testing Library

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["via.placeholder.com", "images.unsplash.com"],
   },
+  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
